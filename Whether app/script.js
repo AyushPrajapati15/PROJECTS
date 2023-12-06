233cb0ec7b4e40cc1eea92816c67ecce05610461
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-12-06 22:34:09 +0530

    Weather script page

---
+++ Whether app/script.js
@@ -0,0 +1,38 @@
+let user_input = document.querySelector('input');
+let btn = document.querySelector('button');
+let icon = document.querySelector('.icon');
+let weatherData = document.querySelector('.weather');
+let temperature = document.querySelector('.temp');
+let description = document.querySelector('desc');
+
+
+
+// 62f4367c410104e4584c2ba7755485b2
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+
+btn.addEventListener('click', () => {
+    let city = user_input.value;
+    getWeatherData(city)
+});
+
+
+
+const getWeatherData = async (city) => {
+    try {
+        
+        const resonse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${`62f4367c410104e4584c2ba7755485b2`}`);
+        const data = await response.json();
+        console.log(resonse);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+};
+
+
+
+
+// GET https://api.openweathermap.org/data/2.5/weather?q=&appid=414d00930df4283f8ed2beefa3ad425b 401 (Unauthorized)
+// getWeatherData @ script.js:22
+// script.js:16Error fetching weather data: ReferenceError: response is not defined
+// at getWeatherData (script.js:25:22)
