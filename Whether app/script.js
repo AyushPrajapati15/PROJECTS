84cda94600b9fdf2ec0710af326a225b726c4fb4
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-12-22 21:23:54 +0530

    Weather script page

--- Whether app/script.js
+++ Whether app/script.js
@@ -3,7 +3,7 @@ let btn = document.querySelector("button");
 let icon = document.querySelector(".icon");
 let weatherData = document.querySelector(".weather");
 let temperature = document.querySelector(".temp");
-let description = document.querySelector("desc");
+let description = document.querySelector(".desc");
 
 // 62f4367c410104e4584c2ba7755485b2
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
@@ -17,7 +17,7 @@ const getWeatherData = async (city) => {
   // try {
 
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${`62f4367c410104e4584c2ba7755485b2`}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=62f4367c410104e4584c2ba7755485b2`
   );
   const data = await response.json();
   console.log(data);
@@ -26,11 +26,13 @@ const getWeatherData = async (city) => {
   // }
 
   const weatherIcon = data.weather[0].icon;
-  icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weatherIcon}.png" alt=weather_image" />`;
+  icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weatherIcon}.png" alt="weather_image" />`;
 
   const cityname = data.name;
   const countryName = data.sys.country;
   weatherData.innerHTML = `${cityname},${countryName}`;
+  const tempdata = data.main.temp;
+  temperature.innerHTML=`${tempdata}`
 };
 
 // script.js:35 Uncaught (in promise) TypeError: Cannot set properties of null (setting 'innerHTML')    at getWeatherData (script.js:35:27)
