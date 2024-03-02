1002963077581568f039c5855323ad37730ba58d
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-03-02 23:57:14 +0530

    update main

--- Q.R code generator python/onlinepayment.py
+++ Q.R code generator python/onlinepayment.py
@@ -4,4 +4,4 @@ upi_id = input("Enter your upi id")
 # upi://pay?pa=UPI_ID&pn=NAME&am=AMOUNT&cu=CURRENCY&tm=MESSAGE
 
 
-phonepe_url=f''
+phonepe_url=f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
