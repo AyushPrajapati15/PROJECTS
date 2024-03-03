1ee1a5e3a6d096c15deb74c39d5509dfb37e794a
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-03-03 23:38:46 +0530

    update main

--- Q.R code generator python/onlinepayment.py
+++ Q.R code generator python/onlinepayment.py
@@ -6,3 +6,7 @@ upi_id = input("Enter your upi id")
 
 phonepe_url=f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
 googlepay_url=f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+
+#Create qr code for each payment app
+phonepe_qr=qrcode.make(phonepe_url)
