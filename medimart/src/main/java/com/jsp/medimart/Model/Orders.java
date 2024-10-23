f55da1092f0616ddb9aa99befd7b8a658f9a3c52
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-10-23 21:31:06 +0530

    Order model

---
+++ medimart/src/main/java/com/jsp/medimart/Model/Orders.java
@@ -0,0 +1,31 @@
+package com.jsp.medimart.Model;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Component
+public class Orders {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int memberid;
+	@OneToMany
+	private List<Drug> drug;
+	private double orderamount;
+
+}
