200f231087a0c1bf30c98d481e65becf73a99f6a
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-11-06 13:08:37 +0530

    Member Dao

--- medimart/src/main/java/com/jsp/medimart/DAO/MemberDao.java
+++ medimart/src/main/java/com/jsp/medimart/DAO/MemberDao.java
@@ -12,52 +12,53 @@ import com.jsp.medimart.Repository.MemberRepo;
 
 @Repository
 public class MemberDao {
-	
+
 	@Autowired
 	MemberRepo memberRepo;
-	
+
 	public Member saveMember(Member member) {
 		return memberRepo.save(member);
 
 	}
+
 	public Member updateMember(Member member) {
 		Optional<Member> op = memberRepo.findById(member.getId());
-		if(op!=null)
-		{
+		if (op != null) {
 			return memberRepo.save(member);
-			
+
 		}
 		return null;
-		
+
 	}
 
 	public Member fetchMember(int id) {
-		Optional<Member> op=memberRepo.findById(id);
-		if(op.isPresent()){
+		Optional<Member> op = memberRepo.findById(id);
+		if (op.isPresent()) {
 			Member member = op.get();
 			return member;
 		}
 		return null;
 	}
+
 	public Member loginMember(String emailid, String password) {
-		Optional<Member> op=memberRepo.findByEmailidAndPassword(emailid, password);
-		if(op.isPresent()){
-			Member admin=op.get();
+		Optional<Member> op = memberRepo.findByEmailidAndPassword(emailid, password);
+		if (op.isPresent()) {
+			Member admin = op.get();
 			return admin;
 		}
 		return null;
 	}
+
 	public Member deleteMember(int id) {
-		Optional<Member> op=memberRepo.findById(id);
-		if(op.isPresent()){
-			Member member=op.get();
+		Optional<Member> op = memberRepo.findById(id);
+		if (op.isPresent()) {
+			Member member = op.get();
 			memberRepo.delete(member);
 			return member;
 		}
 		return null;
 	}
-	
-	
+
 	public List<Member> allMember() {
 		List<Member> all = memberRepo.findAll();
 		return all;
@@ -65,4 +66,4 @@ public class MemberDao {
 	}
 
 }
-//NO-38
+// NO-38
