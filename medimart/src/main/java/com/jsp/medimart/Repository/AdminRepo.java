484f507927ffd38fcc546aa0ee27c3316e874d15
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-11-11 23:16:28 +0530

    Admin Repository

--- medimart/src/main/java/com/jsp/medimart/Repository/AdminRepo.java
+++ medimart/src/main/java/com/jsp/medimart/Repository/AdminRepo.java
@@ -8,15 +8,11 @@ import org.springframework.data.jpa.repository.Query;
 import com.jsp.medimart.Model.Admin;
 import java.util.List;
 
-
 public interface AdminRepo extends JpaRepository<Admin, Integer> {
 
-//	@Query(select a A from a where a.id=?1)
+	// @Query(select a A from a where a.id=?1)
 	Optional<Admin> findById(int id);
-	
-	Optional<Admin> findByEmailidAndPassword(String emailid , String password);
-	
-	
-	
+
+	Optional<Admin> findByEmailidAndPassword(String emailid, String password);
 
 }
